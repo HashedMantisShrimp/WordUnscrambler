2156a43acd0a4165ad5dea17031c2c8d738f4b97
PauloB04 <54282145+PauloB04@users.noreply.github.com>
2019-10-07 02:00:47 +0300

    Added a Unit Test Class
    Added a Unit Test for the main method of the "Unscrambler.cs" Class, the "Matcher" Method. Which is the most important method on the business logic.

---
+++ WordUnscrambler.Test.Unit/WordMatcherTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordUnScrambler.Workers;
+using WordUnScrambler.DataManagement;
+using System.Collections.Generic;
+
+namespace WordUnscrambler.Test.Unit
+{
+    [TestClass]
+    public class WordMatcherTest //Change the name to "UnscramblerTest"?
+    {
+        private readonly Unscrambler _Unscrambler = new Unscrambler();
+        [TestMethod]
+        public void ScrambledWordsArrayMatchesWithTwoWords()
+        {
+            string[] ScrambledWords = { "vElOeV", "quAliFy", "Paragraph", "Glass" };
+            string[] Words = { "Evolve", "Qualify" };
+
+           var _matchedWords = _Unscrambler.Matcher(ScrambledWords, Words);
+
+            Assert.IsTrue(_matchedWords.Count==2);
+            Assert.IsTrue(_matchedWords[0].SramcbledWord.Equals("vElOeV"));
+            Assert.IsTrue(_matchedWords[0].Word.Equals("Evolve"));
+            Assert.IsTrue(_matchedWords[1].SramcbledWord.Equals("quAliFy"));
+            Assert.IsTrue(_matchedWords[1].Word.Equals("Qualify"));
+
+        }
+    }
+}
